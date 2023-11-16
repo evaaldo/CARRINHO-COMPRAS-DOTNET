17552a8e7cc26fbcb54cafb3d4d004d74a3a3f5c
Evaldo Junior <evaaldormj@gmail.com>
2023-11-16 18:52:13 -0300

    Feat: GET

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,10 +1,11 @@
 using CarrinhoCompras.Context;
-using Microsoft.AspNetCore.Components;
+using CarrinhoCompras.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarrinhoCompras.Controller
 {
-    [Route("CarrinhoCompras")]
+    [Route("CarinhoCompras")]
     [ApiController]
     public class ProdutoController : ControllerBase
     {
@@ -13,6 +14,17 @@ namespace CarrinhoCompras.Controller
         public ProdutoController(ProdutoContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
+        {
+            if(_context.Produtos == null)
+            {
+                return NotFound();
+            }
+
+            return await _context.Produtos.ToListAsync();
         }
     }
 }
