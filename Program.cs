a3d73c7c61b488a4f1eae70ff35e10be4fa33d8e
Evaldo Junior <evaaldormj@gmail.com>
2023-11-16 19:16:19 -0300

    Feat: Ligando Program ao Context

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
+using CarrinhoCompras.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<ProdutoContext>(opt => opt.UseInMemoryDatabase("ListaProdutos"));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
